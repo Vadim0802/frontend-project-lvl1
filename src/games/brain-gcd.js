7836f1fd949fd4b8a7e6904310a96c61145c2f9c
Vadim Bodianskii <v.bodyansky@yandex.ru>
2021-02-22 19:04:44 +1000

    Fix: eslint warn

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,4 +1,4 @@
-import { random } from 'lodash';
+import _ from 'lodash';
 import runGameEngine from '../index.js';
 
 const rule = 'Find the greatest common divisor of given numbers.';
@@ -6,8 +6,8 @@ const rule = 'Find the greatest common divisor of given numbers.';
 const findGcd = (x, y) => (y === 0 ? x : findGcd(y, x % y));
 
 const getGameData = () => {
-  const num1 = random(1, 99);
-  const num2 = random(1, 99);
+  const num1 = _.random(1, 99);
+  const num2 = _.random(1, 99);
 
   const question = `${num1} ${num2}`;
   const rigthAnswer = findGcd(num1, num2).toString();
