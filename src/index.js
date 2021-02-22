48cb746247ae7313c9a921e35c3ae959bda6aab3
Vadim Bodianskii <v.bodyansky@yandex.ru>
2021-02-22 15:03:52 +1000

    Refactor: fix runGameEngine()

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ const amountTries = 3;
 const print = (output) => console.log(output);
 
 const runGameEngine = (rule, gameData) => {
-  const [task, rightAnswer] = gameData();
-
   print('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   print(`Hello, ${name}!`);
   print(rule);
 
   for (let i = 0; i < amountTries; i += 1) {
+    const [task, rightAnswer] = gameData();
     print(`Question: ${task}`);
     const answer = readlineSync.question('Your answer: ');
 
