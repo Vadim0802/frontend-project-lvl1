8bb117b180a45acc824a6bf3831413a4109167d7
Vadim Bodianskii <v.bodyansky@yandex.ru>
2021-02-22 14:02:05 +1000

    Add: created gameEngine

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+const amountTries = 3;
+const print = (output) => console.log(output);
+
+const runGameEngine = (rule, gameData) => {
+  const [task, rightAnswer] = gameData();
+
+  print('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  print(`Hello, ${name}!`);
+  print(rule);
+
+  for (let i = 0; i < amountTries; i += 1) {
+    print(`Question: ${task}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer !== rightAnswer) {
+      print(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      print(`Let's try again, ${name}`);
+      return;
+    }
+    print('Correct!');
+  }
+  print(`Congratulations, ${name}`);
+};
+
+export default runGameEngine;
